27db197b64c50f410b75be3b149fcf5ab4ef4029
Ilerioluwa Albert Akinola <ilerioluwaakinola@gmail.com>
2023-01-12 20:08:16 +0200

    Capturing Drone Data

--- Birdnest/drones/util.py
+++ Birdnest/drones/util.py
@@ -1,5 +1,18 @@
 import requests
+import xmltodict
 import json
 import time
 
 # Get Drone Info
+def droneInfo():
+
+    # Getting xml data and turn to json 
+    infoUrl = "http://assignments.reaktor.com/birdnest/drones"
+    treatedXml = requests.get(infoUrl)
+
+    jsonData = xmltodict.parse(treatedXml.text)
+    
+    results = jsonData["report"]["capture"]
+    print(results)
+
+droneInfo()
