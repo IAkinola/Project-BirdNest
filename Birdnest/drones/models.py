a4cd26ee533ba10fac89a227d78ace0b5b8257b0
Ilerioluwa Albert Akinola <ilerioluwaakinola@gmail.com>
2023-01-18 11:36:29 +0200

    Made a database

--- Birdnest/drones/models.py
+++ Birdnest/drones/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class PilotTable(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    phoneNumber = models.IntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
     email = models.CharField(max_length=100)
+    closest_distance = models.IntegerField()
+    ndz_time = models.DateTimeField('Time NDZ Breached')
